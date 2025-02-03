3786df9cd0e4284d8cec8ed2ec57a5a1445338dd
hosain ali <hosainahmed534745@gmail.com>
2025-02-03 15:32:44 +0600

    update

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  // server: { host: '209.97.134.184', port: 8002 },
-  server: { host: '10.0.60.24', port: 5173 },
+  server: { host: '209.97.134.184', port: 8002 },
+  // server: { host: '10.0.60.24', port: 5173 },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
